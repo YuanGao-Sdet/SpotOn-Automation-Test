d4214eb2a9e751166ac908274064531d46fe0aaf
nikiw <summerga2021@gmail.com>
2022-04-12 01:06:22 -0400

    nothing

--- src/test/java/com/practice/pages/Orderpage.java
+++ src/test/java/com/practice/pages/Orderpage.java
@@ -4,5 +4,6 @@ public class Orderpage {
     public static void main(String[] args) {
         System.out.println("this is the code from niki ");
         System.out.println("practice on my code day 2");
+        System.out.println(" ");
     }
 }
