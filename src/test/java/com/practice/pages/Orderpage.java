32afa0a0727efa626d7361b78bb2898baf2ec14d
gaoy <yuangao.sdet@gmail.com>
2022-04-17 23:16:51 -0400

    'updateOneLine'

--- src/test/java/com/practice/pages/Orderpage.java
+++ src/test/java/com/practice/pages/Orderpage.java
@@ -2,8 +2,12 @@ package com.practice.pages;
 
 public class Orderpage {
     public static void main(String[] args) {
-
         System.out.println("this is the code from summer");
         System.out.println("i changed my code ");
+    }
+
+
+    public void newFunction(){
+
     }
 }
