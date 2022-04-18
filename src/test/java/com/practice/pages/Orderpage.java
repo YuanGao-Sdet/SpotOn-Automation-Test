e2aabbf417763034ae869d690496988708f38eb1
nikiw <summerga2021@gmail.com>
2022-04-17 22:40:41 -0400

    changed

--- src/test/java/com/practice/pages/Orderpage.java
+++ src/test/java/com/practice/pages/Orderpage.java
@@ -2,7 +2,7 @@ package com.practice.pages;
 
 public class Orderpage {
     public static void main(String[] args) {
-        System.out.println("this is the code from niki ");
+        System.out.println("this is the code from summer");
         System.out.println("i changed my code ");
 
     }
