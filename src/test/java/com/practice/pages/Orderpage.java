027a2ae66ed99609ee2d82e2294bb4bdbff0cbcf
nikiw <summerga2021@gmail.com>
2022-04-17 22:16:02 -0400

    i changed

--- src/test/java/com/practice/pages/Orderpage.java
+++ src/test/java/com/practice/pages/Orderpage.java
@@ -3,7 +3,7 @@ package com.practice.pages;
 public class Orderpage {
     public static void main(String[] args) {
         System.out.println("this is the code from niki ");
-        System.out.println("practice on my code day 2");
-        System.out.println(" ");
+        System.out.println("i changed my code ");
+
     }
 }
