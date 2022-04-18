6e1f31ff6d85917ee6739181c6c44aa73e133cd3
gaoy <yuangao.sdet@gmail.com>
2022-04-17 22:33:31 -0400

    'practics'

--- src/test/java/com/practice/pages/Orderpage.java
+++ src/test/java/com/practice/pages/Orderpage.java
@@ -3,6 +3,8 @@ package com.practice.pages;
 public class Orderpage {
     public static void main(String[] args) {
         System.out.println("this is the code from niki ");
+        System.out.println(" HEY, WILL BE CONFLICTS ");
         System.out.println("practice on my code day 2");
+
     }
 }
