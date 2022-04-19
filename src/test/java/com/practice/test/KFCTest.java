bb2557e31df86c5d2ee9997b79b261e411395855
kennyfang <kennyfangnyc@gmail.com>
2022-04-19 13:05:06 -0400

    adding order by kf

---
+++ src/test/java/com/practice/test/KFCTest.java
@@ -0,0 +1,10 @@
+package com.practice.test;
+
+public class KFCTest {
+
+    public static void main(String[] args) {
+        System.out.println("Hello KFC");
+        System.out.println("Have a good day");
+        System.out.println("Good night");
+    }
+}
---
+++ src/test/java/com/practice/test/McTest.java
@@ -0,0 +1,7 @@
+package com.practice.test;
+
+public class McTest {
+    public static void main(String[] args) {
+        System.out.println("Hello MC");
+    }
+}
