e687aca5cf88804b3316159bdfe5f824449ee9fc
kenny <94721164+kenny2832@users.noreply.github.com>
2022-07-28 13:06:43 -0400

    Feature/dwa 1457 (#9)
    * adding order by kf
    
    * finished DWA-1457
    
    * new one

---
+++ src/test/java/com/practice/test/McTest.java
@@ -0,0 +1,8 @@
+package com.practice.test;
+
+public class McTest {
+    public static void main(String[] args) {
+        System.out.println("Hello MC");
+        System.out.println("Finish my code");
+    }
+}
